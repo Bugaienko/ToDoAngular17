c64c3ad5aa52d967c19b9632daefafc2bd6e93b4
Bugaienko <sergiibugaienko@gmail.com>
2024-01-10 18:36:50 +0100

    create component factory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {TodoGroupComponent} from "./components/todo-group/todo-group.component";
+import {TodoGroup, TodoStatus} from "../interfaces/todo-group.interface";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TodoGroupComponent,
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ToDo';
+
+  public todoGroups: TodoGroup[];
+
+  constructor() {
+    this.todoGroups = [
+      {
+        title: 'Первый',
+        items: [
+          {
+            title: 'Сделать Todo',
+            description: 'Делаем планировщик задач',
+            status: TodoStatus.IN_PROGRESS
+          },
+        ]
+      }
+    ];
+  }
 }
